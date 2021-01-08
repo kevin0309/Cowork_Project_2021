e9faf25e97215d41afa2ea8286b3c159c812546c
YH <kevin95392004@gmail.com>
2021-01-08 15:25:04 +0900

    selenium test

--- study/crawling/yh/beautifulsoup4.py
+++ study/crawling/yh/beautifulsoup4.py
@@ -11,9 +11,10 @@ html = requests.get(url)
 # async 요청에 대해서 처리할 수 없음 
 # ==> 데이터를 async요청으로 받아오는 페이지일 경우 데이터를 뽑아내지 못함
 # ==> 일반적으로 정적 웹페이지에만 사용 가능 (데이터가 포함된 api에 바로 요청할 수 있다면 사용가능)
+# + 교보문고는 정적웹페이지로 구성되어있음
 soup = BeautifulSoup(html.content, 'html.parser')
 
-#pprint(soup.prettify())
+pprint(soup.prettify())
 
-for link in soup.find_all('a') :
-    pprint(link.get('href'))
+#for link in soup.find_all('a') :
+#    pprint(link.get('href'))
---
+++ study/crawling/yh/selenium_test.py
@@ -0,0 +1,28 @@
+### Required Packages Installation
+# selenium 3은 파이썬 3.9 이상의 최신버전에서 동작하지 않음
+# conda install selenium
+#
+# 사용할 브라우저의 webdriver 파일 필요 ==> 크롬 87버전용 파일 첨부 chromedriver.exe
+
+# 데탑에 설치된 웹 브라우저 어플리케이션의 드라이버를 사용해 브라우저 직접 조작 가능
+# ==> 동적 웹페이지 렌더링 가능
+
+from selenium import webdriver
+
+# 브라우저용 webdriver 로딩
+
+# 에러 로그 발생 ==> USB: usb_device_handle_win.cc:1020 Failed to read descriptor from node connection: 시스템에 부착된 장치가 작동하지 않습니다. (0x1F)
+# 에러 로그 방지용 코드
+# wd_options = webdriver.ChromeOptions()
+# wd_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+# wd = webdriver.Chrome('chromedriver.exe', options=wd_options)
+
+# 일반적인 코드
+wd = webdriver.Chrome('chromedriver.exe')
+wd.implicitly_wait(3)
+
+wd.get('https://www.youtube.com/')
+
+wd.implicitly_wait(3)
+
+print(wd.page_source)
